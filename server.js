db92394e3fc64050905f6dcd53b89fc3edb4eebc
samirgh <lulzylizard@gmail.com>
2017-09-02 14:39:10 +0300

    Dynamic port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express        = require('express');
 const hbs            = require('hbs');
 const fs             = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 // Register Partials
@@ -48,7 +50,7 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server listening to port 3000.');
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
 
